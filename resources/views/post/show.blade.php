79b1d5b36d5b552f6d18403cc7b2e7e9320afd52
ShnigdhaPaul <Shnigdha2000@gmail.com>
2023-05-16 12:33:49 +0600

    Crud method is shown

---
+++ resources/views/post/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+@section('content')
+
+
+<div class="container">
+    <div class="row">
+
+
+      <div class="card">
+        <img class="image-fluid" src="{{url('/storage/images/'.$post->file)}}" alt="Card image cap" height='350', width="250">
+        <div class="card-header">
+      {{ $post->title }} <div>{{ $post->user->name }}</div> <span>{{ $post->created_at->diffForHumans() }}<span>
+        </div>
+        <div class="card-body">
+          <p class="card-text">  {{ $post->content }} </p>
+          <a href="{{ route('post.index') }}" class="btn btn-success">Back</a>
+        </div>
+      </div>
+    </div>
+
+<div class="container p-3 text-center">
+    <form action="{{ route('comment.store', $post->id) }}" method="post">
+@csrf
+<textarea rows="5" cols="50" name="content" class="form-controller"></textarea>
+<div><button type="submit" class="btn btn-danger">add</button></div>
+    </form>
+</div><br>
+
+@foreach ($comments as $comment )
+
+<div class="card">
+    <div class="card-header">
+  {{ $comment->user->name }} <span>{{ $comment->created_at->diffForHumans() }}<span>
+    </div>
+    <div class="card-body">
+      <p class="card-text">  {{ $comment->content }} </p>
+    </div>
+  </div>
+  @endforeach
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::resource('/post', PostController::class);
+
+Route::post('/comment/store/{pid}', [CommentController::class, 'store'])->name('comment.store');
+
+Route::get('/dashboard', [PostController::class, 'dashboard'])->name('dashboard');
